8f35cea19cfa3f8552404272aef5eba20271c5d5
Mahx2019 <1491657425@qq.com>
2022-07-29 18:40:23 +0800

    Update resource.js

--- js/resource.js
+++ js/resource.js
@@ -6,21 +6,19 @@ export const storage2 = {
     "cites":[
         {
             words: ["海上升明月，天涯共此时。"],
-            colors: ["#4b78da"],
             keywords: ["明", "月", "天", "涯"],
         },
         {
             words: ["落霞与孤鹜齐飞，秋水共长天一色。"],
-            colors: ["#4b78da"],
             keywords: ["孤", "鹜", "秋", "水"],
         },
         {
             words: ["人生代代无穷己，江月年年只相似。"],
-            colors: ["#4b78da"],
+
             keywords: ["无", "穷", "己", "只", "相", "似"],
         },
-]
-
+    ],
+    "colors":["#4b78da", "#55E0BDBE"]
 };
 
 
