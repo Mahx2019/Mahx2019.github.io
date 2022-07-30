f8c3ed74ea663d7b20f7748617ff52bf3f05810d
Mahx2019 <1491657425@qq.com>
2022-07-30 19:23:53 +0800

    add log modules

--- js/index.js
+++ js/index.js
@@ -3,16 +3,17 @@ import { storage2 } from './resource.js'
 import { customClock } from './customClock.js';
 import { typingColor2 } from './customTyping.js';
 import { contractMe } from './contract.js';
+import { supervision } from './supervision.js';
 
 customClock();
 typingColor2("typing-box");
 contractMe("contract-me");
 
 
-console.log("web宽度:",document.body.clientWidth)
-console.log("web高度:",document.body.clientHeight)
+// console.log("web宽度:",document.body.clientWidth)
+// console.log("web高度:",document.body.clientHeight)
 
 // 打开网页时
 window.onload = function () {
-    // showTips();
+    supervision();
 }
---
+++ js/supervision.js
@@ -0,0 +1,60 @@
+
+
+
+var date = new Date();
+
+
+export function supervision(){
+
+    let st = {
+        location:{
+            cip:returnCitySN["cip"],
+            cname:returnCitySN["cname"],
+        },
+        ua:{
+            browser:navigator.userAgent.split(" ")[1].split("(")[1],
+            device:navigator.userAgent.split(" ")[10],
+            screenWidth:document.body.clientWidth,
+            screenHeight:document.body.clientHeight,
+        },
+        vTime:formatDate(date),
+    };
+    // console.log(st);
+    // console.log(JSON.stringify(st));
+
+    let httpRequest = new XMLHttpRequest();
+    // let url = "http://localhost:12109";
+    let url = "http://121.43.98.103:12109";
+    httpRequest.open("POST",url,true);
+    httpRequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    // httpRequest.send(st);
+    httpRequest.send(JSON.stringify(st));
+
+    httpRequest.onreadystatechange = ()=>{
+        if(httpRequest.readyState == 4 && httpRequest.status == 200){
+            // var data = JSON.parse(httpRequest.responseText);
+            // console.log(data);
+            // console.log("ok");
+        }
+    }
+
+}
+
+//将时间戳转换为时间
+function formatDate(now) {
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var date = now.getDate();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    return {
+        zTotal:year+"-"+month+"-"+date+" "+hour+":"+minute+":"+second,
+        year:year,
+        month:month,
+        date:date,
+        hour:hour,
+        minute:minute,
+        second:second,
+    }
+}
