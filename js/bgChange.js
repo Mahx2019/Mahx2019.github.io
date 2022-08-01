38e1865af0570ba6f27d5446ef9f4a287b425fdb
Mahx2019 <1491657425@qq.com>
2022-08-01 23:43:32 +0800

    redesign the index.html and some styles

--- js/bgChange.js
+++ js/bgChange.js
@@ -1,7 +1,7 @@
 
 
 export function getNowHourImage() {
-    let bg = document.getElementsByClassName('shade-bg')[0];
+    let bg = document.getElementsByClassName('s-section')[0];
     let now = new Date();
     let hour = now.getHours();
     if( hour >= 8 && hour <= 19){
